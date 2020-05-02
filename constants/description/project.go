c8086231d8dd09d8d202141d5a5cbfcbf11bd0fe
programmer-richa <programmer.richa@gmail.com>
2020-05-02 11:01:55 +0530

    Project description constants.

---
+++ constants/description/project.go
@@ -0,0 +1,11 @@
+// Package description lists the details regarding project
+package description
+
+const (
+	// Author stores the name of project developer
+	Author = "Richa"
+	// Date stores the date of project development
+	Date = "May 2, 2020"
+	// Description stores a short description of the project
+	Description = "A utility package is not self-executable, instead, it enhances the functionality of an executable package by providing utility functions and other important assets."
+)
