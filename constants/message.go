cd9fdbfc41b6003729a6d11454c42baa32168625
programmer-richa <programmer.richa@gmail.com>
2020-05-02 11:02:36 +0530

    Project error and success messages.

---
+++ constants/message.go
@@ -0,0 +1,16 @@
+// Package message contains custom messages to be used in the program
+package message
+
+// All the constant starting with capital letter are exported constants
+// constant block contains list of error messages
+const (
+
+	// LoginErr prompts login error message.
+	LoginErr = "Unable to Login"
+)
+
+// constant block contains list of success messages
+const (
+	// LoginSucc prompts login success message.
+	LoginSucc = "Login Successful."
+)
